bb1a6a7eff83e61f4a5a79bb4b8f5d991cbf0e0f
Lakshya Gupta <144316580+Lakshya2411@users.noreply.github.com>
2026-05-15 22:25:38 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+
+        vector<int> result;
+
+        for(int i = 0; i < temperatures.size(); i++) {
+
+            bool found = false;
+
+            for(int j = i + 1; j < temperatures.size(); j++) {
+
+                if(temperatures[j] > temperatures[i]) {
+
+                    result.push_back(j - i);
+
+                    found = true;
+
+                    break;
+                }
+            }
+
+            if(found == false) {
+                result.push_back(0);
+            }
+        }
+
+        return result;
+    }
+};
